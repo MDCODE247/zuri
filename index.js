5b6e750ee9db80b3e78beecf23837c4bef55bbad
MD <ammedabubakard500@gmail.com>
2024-05-09 10:42:13 +0100

    calculator

---
+++ index.js
@@ -0,0 +1,206 @@
+const display = document.querySelector("#display");
+const numBtn = document.querySelectorAll(".numBtn");
+numBtn.forEach((button) => button.addEventListener("click", handleNum));
+const operateBtn = document.querySelectorAll(".operateBtn");
+operateBtn.forEach((button) => button.addEventListener("click", handleOp));
+const decimal = document.querySelector("#decimal");
+decimal.addEventListener("click", inputDecimal);
+const equals = document.querySelector("#equals");
+equals.addEventListener("click", inputEquals);
+const backspace = document.querySelector("#backspace");
+backspace.addEventListener("click", removeLastChar);
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", () => {
+    display.setAttribute("style", "border-color: #333333;");
+    display.textContent = "0";
+    firstNum = "";
+    lastNum = "";
+    operator = "";
+});
+
+// Inputs
+let firstNum = "";
+let lastNum = "";
+let operator = "";
+
+// Begin Basic Functions
+const add = function (a, b) {
+    return a + b;
+};
+const subtract = function (a, b) {
+    return b - a;
+};
+const multiply = function (a, b) {
+    return a * b;
+};
+const divide = function (a, b) {
+    if (a === 0) {
+        return "LOL";
+    } else {
+        return b / a;
+    }
+};
+// ------------->
+
+// Operate
+
+function operate() {
+    let result;
+
+    a = parseFloat(firstNum);
+    b = parseFloat(lastNum);
+
+    if (operator == "+") {
+        result = add(a, b);
+    } else if (operator == "-") {
+        result = subtract(a, b);
+    } else if (operator == "x") {
+        result = multiply(a, b);
+    } else if (operator == "÷") {
+        result = divide(a, b);
+    }
+
+    if (a === "" || b === "" || operator === "") {
+        firstNum = "";
+        lastNum = "";
+        operator = "";
+        result = 0;
+    }
+
+    if (result.toString().includes(".")) {
+        result = parseFloat(result.toFixed(2));
+    }
+
+    if (isNaN(result) && result != "LOL") {
+        result = 0;
+    }
+
+    display.textContent = result;
+    return result;
+}
+// ------------->
+
+let isEqualsPressed = false;
+
+function inputEquals() {
+    operate();
+    firstNum = operate();
+    lastNum = "";
+    isEqualsPressed = true;
+    display.setAttribute("style", "border-color: #333333;");
+}
+
+function handleNum(event) {
+    let displayContent = display.textContent.replace(/\./g, "");
+    if (displayContent.length >= 8) {
+        display.setAttribute(
+            "style",
+            "border-color: #8b0000; transform: scale(1.02);"
+        );
+        return;
+    }
+
+    if (isEqualsPressed && !isNaN(event.target.textContent)) {
+        display.textContent = "";
+        isEqualsPressed = false;
+        firstNum = "";
+        lastNum = "";
+        operator = "";
+    } else if (display.textContent == 0) {
+        display.textContent = "";
+        isEqualsPressed = false;
+    }
+    display.textContent += event.target.textContent;
+    let input = event.target.textContent;
+    firstNum += input;
+}
+
+function isOperator(char) {
+    const operators = ["+", "-", "x", "÷"];
+    return operators.includes(char);
+}
+
+function handleOp(event) {
+    let displayContent = display.textContent.replace(/\./g, "");
+    if (displayContent.length >= 7 && !isOperator(event.target.textContent)) {
+        return;
+    }
+
+    if (
+        firstNum === "" &&
+        lastNum === "" &&
+        (event.target.textContent === "-" || event.target.textContent === "+")
+    ) {
+        firstNum += event.target.textContent;
+        operator = "";
+        display.textContent = firstNum;
+        return;
+    }
+
+    if (isEqualsPressed && isOperator(event.target.textContent)) {
+        display.textContent += `${operator}`;
+        lastNum = firstNum;
+        operator = event.target.textContent;
+        firstNum = "";
+        isEqualsPressed = false;
+    }
+
+    if (operator === "") {
+        operator = event.target.textContent;
+        if (display.textContent == 0) {
+            display.textContent = "";
+        }
+        display.textContent += `${operator}`;
+        lastNum = firstNum; // Sets the first operand equal to the numbers before the operator
+        firstNum = "";
+    } else {
+        const lastChar = display.textContent.slice(-1);
+        if (isOperator(lastChar)) {
+            display.textContent =
+                display.textContent.slice(0, -1) + event.target.textContent;
+            operator = event.target.textContent;
+        } else {
+            operate();
+            display.setAttribute("style", "border-color: #333333;");
+            lastNum = operate();
+            operator = event.target.textContent;
+            display.textContent += `${operator}`;
+            firstNum = "";
+        }
+    }
+}
+
+function inputDecimal() {
+    if (display.textContent.includes(".") && firstNum.includes(".")) {
+        return;
+    }
+    if (firstNum.includes(".") && lastNum.includes(".")) {
+        return;
+    }
+    if (display.textContent == 0) {
+        display.textContent = "";
+    }
+    firstNum += event.target.textContent;
+    display.textContent += event.target.textContent;
+}
+
+function removeLastChar() {
+    if (display.textContent == 0) {
+        return;
+    } else {
+        firstNum = firstNum.toString().slice(0, -1);
+    }
+
+    if (isOperator(display.textContent.slice(-1))) {
+        operator = "";
+        firstNum = lastNum; // Reverses the last operand back to the numbers before the operator (see handleOP()).
+        lastNum = "";
+        display.textContent = firstNum;
+    } else if (lastNum != "") {
+        display.textContent = `${lastNum}${operator}${firstNum}`;
+    } else {
+        display.textContent = firstNum;
+    }
+
+    display.setAttribute("style", "border-color: #333333;");
+}
